07d1e3c09d32e7c550b0da39f371624e8e0d20b3
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-08-31 00:00:39 +0100

    (relation managers): city and department resource relation manager - employees

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\DepartmentResource\Pages;
 use App\Filament\Resources\DepartmentResource\RelationManagers;
+use App\Filament\Resources\DepartmentResource\RelationManagers\EmployeesRelationManager;
 use App\Models\Department;
 use Filament\Forms;
 use Filament\Forms\Components\Section;
@@ -64,7 +65,7 @@ class DepartmentResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+            EmployeesRelationManager::class
         ];
     }
 
