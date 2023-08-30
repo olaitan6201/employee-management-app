22e3a8da9193104f929d17d1c33cf5005cce5a01
{{Dev.Habeeb}} <olaitan6201@gmail.com>
2023-08-30 23:58:04 +0100

    (relation manager): State Resource relation managers (employees, cities)

---
+++ app/Filament/Resources/StateResource/RelationManagers/EmployeesRelationManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Filament\Resources\StateResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EmployeesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'employees';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('firstname')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('firstname')
+            ->columns([
+                Tables\Columns\TextColumn::make('firstname'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+}
